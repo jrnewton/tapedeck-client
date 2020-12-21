2a2325c31a5146c52b9f09101c7dace42b35be38
Jon Newton <newt@hey.com>
2020-12-21 14:32:53 -0500

    A really simple smoke test for the archives page

--- cypress/integration/smoke_test.js
+++ cypress/integration/smoke_test.js
@@ -1,5 +1,6 @@
-describe('just the basics', () => {
-  it('first test', () => {
-    expect(true).to.equal(true);
+describe('basic smoke tests', () => {
+  it('should load archive page via button click', () => {
+    cy.visit('/').get('[data-cy=yourArchivesButton]').click();
+    cy.url().should('include', '/archives.html');
   });
 });
