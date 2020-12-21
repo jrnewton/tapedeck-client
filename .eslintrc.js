d952e432672b47f6e341e54ceaa1c9d4ca28f44c
Jon Newton <newt@hey.com>
2020-12-20 21:47:19 -0500

    Update eslint for browser code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
   env: {
-    commonjs: true,
-    es2020: true,
-    node: true,
+    es2018: true,
     mocha: true,
     browser: true
   },
   /* eslint:recommended property enables rules that report common problems 
        See the list at https://eslint.org/docs/rules/ */
   /* eslint/prettier integration read https://bit.ly/3kQFe3u */
-  extends: ['eslint:recommended', 'prettier'], //, 'plugin:vue/vue3-recommended'],
+  extends: ['eslint:recommended', 'prettier', 'plugin:vue/vue3-recommended'],
   rules: {
     'strict': 'error',
     'semi': 'error',
     'prettier/prettier': 'error'
   },
-  plugins: ['prettier'] //, 'vue']
+  plugins: ['prettier', 'vue']
 };
