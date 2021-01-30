150afc87543a10add2a113da4b7cca589c76b497
Jon Newton <newt@hey.com>
2021-01-30 00:55:22 -0500

    Put all auth pages into the main app in a modal dialog.

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -7,44 +7,174 @@
         </h1>
         <p class="lead">Record radio streams to the cloud</p>
       </div>
-
-      <!--
-      <aside class="card col-12 col-lg-4 d-none d-sm-block">
-        <div class="card-body">
-          <h4 class="card-title">About</h4>
-          <p class="card-text">
-            I &#128151; <a href="https://wmbr.org/">college radio</a> and wrote
-            this app to capture my favorite shows for later listening and
-            archiving.
-          </p>
-          <a href="https://github.com/jrnewton/tapedeck" class="card-link"
-            >View the code on GitHub</a
-          >
-        </div>
-      </aside>
-      -->
     </div>
 
     <hr class="my-4" />
 
     <div id="main">
-      <form id="capture-form" @submit.prevent="archiveSubmit">
-        <div class="form-group">
-          <!-- 
-            <div class="my-3">
-              <input
-                type="email"
-                class="form-control form-control-lg"
-                id="emailInput"
-                aria-describedby="emailHelp"
-                placeholder="Your email"
-                v-model="email"
-              />
-              <small id="emailHelp" class="form-text text-muted"
-                >Enter your email address.</small
+      <teleport to="#overlay">
+        <div id="overlayarea" @click="overlayClose"></div>
+        <dialog open v-if="showOverlay">
+          <form
+            id="login-form"
+            @submit.prevent="signin"
+            v-if="overlayContent === 'login'"
+          >
+            <div class="form-group">
+              <div class="my-3">
+                <input
+                  type="text"
+                  class="form-control form-control-lg"
+                  id="usernameInput"
+                  aria-describedby="usernameHelp"
+                  placeholder="Username"
+                  v-model="username"
+                />
+                <small id="usernameHelp" class="form-text text-muted"
+                  >Enter your username</small
+                >
+              </div>
+              <div class="my-3">
+                <input
+                  type="password"
+                  class="form-control form-control-lg"
+                  id="passwordInput"
+                  aria-describedby="passwordHelp"
+                  placeholder="Password"
+                  v-model="password"
+                />
+                <small id="passwordHelp" class="form-text text-muted"
+                  >Enter your password</small
+                >
+              </div>
+
+              <button
+                name="loginButton"
+                class="btn btn-primary btn-lg my-3"
+                href="#"
+                role="button"
+                type="submit"
+              >
+                Login
+              </button>
+
+              <a
+                href="#"
+                @click="overlayContent = 'signup'"
+                class="my-3 mx-3"
+                data-cy="createAccountButton"
+                >Create an account</a
+              >
+
+              <a
+                href="#"
+                @click="overlayContent = 'confirm'"
+                class="my-3 mx-3"
+                data-cy="confirmAccountButton"
+                >Enter code</a
               >
             </div>
-            -->
+          </form>
+
+          <form
+            id="signup-form"
+            @submit.prevent="signup"
+            v-if="overlayContent === 'signup'"
+          >
+            <div class="form-group">
+              <div class="my-3">
+                <input
+                  type="email"
+                  class="form-control form-control-lg"
+                  id="emailInput"
+                  aria-describedby="emailHelp"
+                  placeholder="Email"
+                  v-model="email"
+                />
+                <small id="emailHelp" class="form-text text-muted"
+                  >Enter your email address</small
+                >
+              </div>
+              <div class="my-3">
+                <input
+                  type="text"
+                  class="form-control form-control-lg"
+                  id="usernameInput"
+                  aria-describedby="usernameHelp"
+                  placeholder="Username"
+                  v-model="username"
+                />
+                <small id="usernameHelp" class="form-text text-muted"
+                  >Enter your desired username</small
+                >
+              </div>
+              <div class="my-3">
+                <input
+                  type="password"
+                  class="form-control form-control-lg"
+                  id="passwordInput"
+                  aria-describedby="passwordHelp"
+                  placeholder="Password"
+                  v-model="password"
+                />
+                <small id="passwordHelp" class="form-text text-muted"
+                  >Enter your desired password</small
+                >
+              </div>
+              <button
+                name="signupButton"
+                class="btn btn-primary btn-lg my-3"
+                href="#"
+                role="button"
+                type="submit"
+              >
+                Create
+              </button>
+            </div>
+          </form>
+
+          <form
+            id="confirm-form"
+            @submit.prevent="confirm"
+            v-if="overlayContent === 'confirm'"
+          >
+            <input
+              type="text"
+              class="form-control form-control-lg"
+              id="usernameInput"
+              aria-describedby="usernameHelp"
+              placeholder="Username"
+              v-model="username"
+            />
+            <small id="usernameHelp" class="form-text text-muted"
+              >Enter your username</small
+            >
+            <input
+              type="text"
+              class="form-control form-control-lg"
+              id="codeInput"
+              aria-describedby="codeHelp"
+              placeholder="Confirmation code"
+              v-model="code"
+            />
+            <small id="codeHelp" class="form-text text-muted"
+              >Enter your confirmation code</small
+            >
+            <button
+              name="confirmButton"
+              class="btn btn-primary btn-lg my-3"
+              href="#"
+              role="button"
+              type="submit"
+            >
+              Confirm
+            </button>
+          </form>
+        </dialog>
+      </teleport>
+
+      <form id="capture-form" @submit.prevent="archiveSubmit">
+        <div class="form-group">
           <div class="my-3">
             <input
               type="text"
@@ -67,8 +197,11 @@
           >
             Archive Now
           </button>
+
+          <span>{{ status }}</span>
         </div>
       </form>
+
       <p class="lead">
         <a
           class="btn btn-outline-info btn-lg my-3"
@@ -76,22 +209,6 @@
           href="archives.html"
           role="button"
           >Your Archives</a
-        >
-      </p>
-      <p class="lead">
-        <a
-          class="btn btn-outline-info btn-lg my-3"
-          data-cy="loginButton"
-          href="auth-login.html"
-          role="button"
-          >Login</a
-        >
-        <a
-          class="btn btn-outline-info btn-lg my-3"
-          data-cy="createAccountButton"
-          href="auth-create.html"
-          role="button"
-          >Create new account</a
         >
       </p>
     </div>
@@ -105,47 +222,153 @@
 </template>
 
 <script>
-import axios from 'axios';
+//import axios from 'axios';
+import Amplify from '@aws-amplify/core';
+import Auth from '@aws-amplify/auth';
+import config from '../../awsconfig';
 
 export default {
   data() {
     return {
       email: '',
       archiveURL: '',
-      status: ''
+      status: '',
+      username: '',
+      password: '',
+      code: null,
+      currentUser: null,
+      showOverlay: false,
+      overlayContent: null //one of login, signup, confirm
     };
   },
+  mounted() {
+    Amplify.configure(config);
+  },
   methods: {
+    overlayClose() {
+      this.showOverlay = false;
+      this.overlayContent = null;
+    },
+    async signin() {
+      console.log('calling signin');
+
+      try {
+        const params = {
+          username: this.username,
+          password: this.password
+        };
+        const { user } = await Auth.signIn(params);
+        console.log('signin result', user);
+        this.currentUser = user;
+        console.log('currentUser set to', this.currentUser);
+
+        this.overlayClose();
+        this.archiveSubmit();
+
+        console.log('done');
+      } catch (error) {
+        console.log('signin error', error);
+      }
+    },
+    async signup() {
+      console.log('calling signup');
+
+      try {
+        const params = {
+          username: this.username,
+          password: this.password,
+          attributes: {
+            email: this.email
+          }
+        };
+        const { user } = await Auth.signUp(params);
+        console.log('signUp result', user);
+        this.currentUser = user;
+        console.log('currentUser set to', this.currentUser);
+
+        this.overlayContent = 'confirm';
+
+        console.log('redirected to auth confirm');
+      } catch (error) {
+        console.log('signUp error', error);
+      }
+    },
+    async confirm() {
+      console.log('confirm called');
+      try {
+        const result = await Auth.confirmSignUp(this.username, this.code);
+        console.log('confirm result', result);
+
+        this.overlayClose();
+        this.archiveSubmit();
+      } catch (error) {
+        console.log('confirm error', error);
+      }
+
+      console.log('confirm returning');
+    },
     async archiveSubmit() {
       if (this.archiveURL === '') {
         alert('Please enter a URL to archive');
         return;
       }
 
-      try {
-        //TODO: add real URL once auth is in place
-        const res = await axios.get('lambda-url');
-        console.log(JSON.stringify(res));
-      } catch (error) {
-        console.log(error);
+      if (this.currentUser === null) {
+        this.overlayContent = 'login';
+        this.showOverlay = true;
+        return;
       }
+
+      this.status = 'Downloading ' + this.archiveURL;
+      setTimeout(() => {
+        this.status = null;
+      }, 3000);
+
+      // try {
+      //   //TODO: add real URL once auth is in place
+      //   const res = await axios.get('https://www.google.com');
+      //   console.log(JSON.stringify(res));
+      // } catch (error) {
+      //   console.log(error);
+      // }
+
       this.archiveURL = '';
     }
-  },
-  computed: {}
+  }
 };
 </script>
 
 <style>
 @import '../../assets/sketchy.min.css';
 
-/* the default styles setup by vue-cli */
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* #overlayarea {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.75);
+  z-index: 10;
 } */
+
+dialog {
+  position: fixed;
+  top: 40vh;
+  left: 30%;
+  width: 40%;
+  z-index: 100;
+  border-radius: 12px;
+  border: none;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+}
+
+@media (min-width: 768px) {
+  dialog {
+    left: calc(50% - 20rem);
+    width: 40rem;
+  }
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -4,22 +4,7 @@ module.exports = {
       entry: './src/pages/index/main.js',
       title: 'Tape Deck - record radio streams to the cloud'
     },
-    authLogin: {
-      entry: './src/pages/auth-login/main.js',
-      title: 'Tape Deck - login to your account',
-      filename: 'auth-login.html'
-    },
-    authCreate: {
-      entry: './src/pages/auth-create/main.js',
-      title: 'Tape Deck - create an account',
-      filename: 'auth-create.html'
-    },
-    authConfirm: {
-      entry: './src/pages/auth-confirm/main.js',
-      title: 'Tape Deck - confirm your new account',
-      filename: 'auth-confirm.html'
-    },
-    archive: {
+    archives: {
       entry: './src/pages/archives/main.js',
       title: 'Tape Deck - Your Archives',
       filename: 'archives.html'
