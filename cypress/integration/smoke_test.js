8843a7aa24a036e7693b0718db99a9e778639a49
Jon Newton <newt@hey.com>
2021-01-30 21:08:25 -0500

    No tests right now

--- cypress/integration/smoke_test.js
+++ cypress/integration/smoke_test.js
@@ -1,6 +1,6 @@
 describe('basic smoke tests', () => {
-  it('should load archive page via button click', () => {
-    cy.visit('/').get('[data-cy=yourArchivesButton]').click();
-    cy.url().should('include', '/archives.html');
-  });
+  // it('should load archive page via button click', () => {
+  //   cy.visit('/').get('[data-cy=yourArchivesButton]').click();
+  //   cy.url().should('include', '/archives.html');
+  // });
 });
