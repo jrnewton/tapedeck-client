8eb0370d6bcaac8ef656dff1eeb0e73a0b36e616
Jon Newton <newt@hey.com>
2021-04-20 15:07:07 -0400

    localStorage util to ensure prefix is used for all keys.

---
+++ src/localStorage.js
@@ -0,0 +1,13 @@
+const prefix = 'tapedeck';
+
+export const setItem = (key, value) => {
+  localStorage.setItem(`${prefix}-${key}`, value);
+};
+
+export const getItem = (key) => {
+  return localStorage.getItem(`${prefix}-${key}`);
+};
+
+export const removeItem = (key) => {
+  localStorage.remoteItem(`${prefix}-${key}`);
+};
